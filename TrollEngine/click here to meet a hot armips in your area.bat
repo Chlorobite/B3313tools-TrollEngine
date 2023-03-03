a14eb4d2e545392a55ba705a9999330708aabde5
Chlorobite <18270989+Chlorobite@users.noreply.github.com>
2023-03-03 08:17:10 +0100

    Update armips.exe and .bat file, Windows works now

--- TrollEngine/click here to meet a hot armips in your area.bat
+++ TrollEngine/click here to meet a hot armips in your area.bat
@@ -1,8 +1,8 @@
-armips "apply deez balls in your mouth eediot.asm"
-n64crc.exe "../Bee/b3313 new.z64"
+.\armips.exe "apply deez balls in your mouth eediot.asm"
+.\n64crc.exe "..\Bee\b3313 new.z64"
 copy "..\Bee\b3313 silved.config" "..\Bee\b3313 new.config"
 del /f "..\Bee\b3313 silved.z64"
 del /f "..\Bee\b3313 silved.config"
-rename "..\Bee\b3313 new.z64" "..\Bee\b3313 silved.z64"
-rename "..\Bee\b3313 new.config" "..\Bee\b3313 silved.config"
+move "..\Bee\b3313 new.z64" "..\Bee\b3313 silved.z64"
+move "..\Bee\b3313 new.config" "..\Bee\b3313 silved.config"
 pause
