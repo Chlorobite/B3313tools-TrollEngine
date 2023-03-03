504f6f4173f84a8d71e88ac432ffd8702cda4ad6
Chlorobite <18270989+Chlorobite@users.noreply.github.com>
2023-03-03 06:42:08 +0100

    Fix armips bat paths

--- TrollEngine/click here to meet a hot armips in your area.bat
+++ TrollEngine/click here to meet a hot armips in your area.bat
@@ -3,6 +3,6 @@ n64crc.exe "../Bee/b3313 new.z64"
 copy "..\Bee\b3313 silved.config" "..\Bee\b3313 new.config"
 del /f "..\Bee\b3313 silved.z64"
 del /f "..\Bee\b3313 silved.config"
-rename "..\Bee\b3313 new.z64" "b3313 silved.z64"
-rename "..\Bee\b3313 new.config" "b3313 silved.config"
+rename "..\Bee\b3313 new.z64" "..\Bee\b3313 silved.z64"
+rename "..\Bee\b3313 new.config" "..\Bee\b3313 silved.config"
 pause
