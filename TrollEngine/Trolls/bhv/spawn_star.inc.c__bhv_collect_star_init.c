8d1ebc083565514d5c02601c363144ddb527132f
Chlorobite <18270989+Chlorobite@users.noreply.github.com>
2023-03-06 20:44:45 +0100

    Update rom, make topbhv stars in red/green star areas high by default

--- TrollEngine/Trolls/bhv/spawn_star.inc.c__bhv_collect_star_init.c
+++ TrollEngine/Trolls/bhv/spawn_star.inc.c__bhv_collect_star_init.c
@@ -4,13 +4,20 @@
 u32 blue_star_check(s32 behParams);
 
 void bhv_collect_star_init(void) {
-    o->header.gfx.sharedChild = gLoadedGraphNodes[blue_star_check(o->oBehParams) ? MODEL_TRANSPARENT_STAR : MODEL_STAR];
+    register struct Object *_o = o;
+
+    if (personalization_beeparams & 0x60) { // red and green star flags
+        // as red and green stars are all in the high region, set the high flag
+        _o->oBehParams |= 0x100;
+    }
+
+    _o->header.gfx.sharedChild = gLoadedGraphNodes[blue_star_check(_o->oBehParams) ? MODEL_TRANSPARENT_STAR : MODEL_STAR];
 
     // rom manger stuff?
-    o->oFlags = 1;
-    o->header.gfx.node.flags |= 4;
+    _o->oFlags = 1;
+    _o->header.gfx.node.flags |= 4;
 
-    obj_set_hitbox(o, (void*)0x80332784);
-    AI_star_set_bparams(o);
+    obj_set_hitbox(_o, (void*)0x80332784);
+    AI_star_set_bparams(_o);
 }
 
