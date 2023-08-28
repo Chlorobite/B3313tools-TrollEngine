799acceb675f80b919d2b328ad697c49f6d6d5d7
Chlorobite <18270989+Chlorobite@users.noreply.github.com>
2023-08-28 13:23:18 +0200

    fix betaheal

--- TrollEngine/Mario/Trolls/mario/update_mario_health.c
+++ TrollEngine/Mario/Trolls/mario/update_mario_health.c
@@ -14,19 +14,21 @@ void update_mario_health(struct MarioState *m) {
                 if (!(m->flags & MARIO_METAL_CAP)) {
                     m->health -= 4;
                 }
-            } else {
-                if ((m->action & ACT_FLAG_SWIMMING) && !(m->action & ACT_FLAG_INTANGIBLE)) {
-                    terrainIsSnow = (m->area->terrainType & TERRAIN_MASK) == TERRAIN_SNOW;
+            } else if ((m->action & ACT_FLAG_SWIMMING) && !(m->action & ACT_FLAG_INTANGIBLE)) {
+                terrainIsSnow = (m->area->terrainType & TERRAIN_MASK) == TERRAIN_SNOW;
 
-                    // When Mario is near the water surface, recover health (unless in snow),
-                    // when in snow terrains lose 3 health.
-                    // If using the debug level select, do not lose any HP to water.
-                    if ((m->pos[1] >= (m->waterLevel - 140)) && !terrainIsSnow) {
-                        m->health += 0x1A;
-                    } else {
-                        m->health -= (terrainIsSnow ? 3 : 1);
-                    }
+                // When Mario is near the water surface, recover health (unless in snow),
+                // when in snow terrains lose 3 health.
+                // If using the debug level select, do not lose any HP to water.
+                if ((m->pos[1] >= (m->waterLevel - 140)) && !terrainIsSnow) {
+                    m->health += 0x1A;
+                } else {
+                    m->health -= (terrainIsSnow ? 3 : 1);
                 }
+            }
+            else if (TRACKER_difficulty_modifier <= 1.5f) {
+                // betaheal
+                gMarioState->health += 1;
             }
         }
 
@@ -37,11 +39,6 @@ void update_mario_health(struct MarioState *m) {
         if (m->hurtCounter > 0) {
             m->health -= 0x40;
             m->hurtCounter--;
-        }
-
-        if (TRACKER_difficulty_modifier <= 1.5f) {
-            // betaheal
-            gMarioState->health += 1;
         }
 
         if (m->health > 0x880) {
