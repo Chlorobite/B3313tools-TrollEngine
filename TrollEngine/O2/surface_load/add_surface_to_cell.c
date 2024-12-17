b95c36999cbd4dfc521dd84313fbe1f52aad0e71
Chlorobite <18270989+Chlorobite@users.noreply.github.com>
2024-12-17 11:01:48 +0100

    replace invisible walls with visible floralys & motos fix

--- TrollEngine/O2/surface_load/add_surface_to_cell.c
+++ TrollEngine/O2/surface_load/add_surface_to_cell.c
@@ -3,34 +3,42 @@
 f32 absf(f32 a);
 
 void add_surface_to_cell(s32 dynamic, s16 cellX, s16 cellZ, struct Surface *surface) {
-    register struct SurfaceNode *newNode = alloc_surface_node();
-    register struct SurfaceNode *list;
-    register s32 priority;
-    register s32 sortDir;
-    register s32 listIndex = SPATIAL_PARTITION_FLOORS;
-
-    if (surface->normal.y <= 0.05f) {
-        if (surface->normal.y < -0.05f) {
-            listIndex = SPATIAL_PARTITION_CEILS;
-        } else {
-            listIndex = SPATIAL_PARTITION_WALLS;
-
-            if (absf(surface->normal.x) > 0.707f)
-                surface->flags |= SURFACE_FLAG_X_PROJECTION;
-        }
-    }
-
-    newNode->surface = surface;
+    register struct SurfaceNode *cell;
+    register f32 normalY = surface->normal.y;
 
     if (dynamic) {
 #ifdef TROLLDEBUG
         dynamicSurfaceTris++;
 #endif
-        list = &gDynamicSurfacePartition[cellZ][cellX][listIndex];
+        cell = gDynamicSurfacePartition[cellZ][cellX];
     } else {
-        list = &gStaticSurfacePartition[cellZ][cellX][listIndex];
+        cell = gStaticSurfacePartition[cellZ][cellX];
     }
 
-    newNode->next = list->next;
-    list->next = newNode;
+    if (normalY < -0.05f) {
+        register struct SurfaceNode *newNode = alloc_surface_node();
+        register struct SurfaceNode *list = &cell[SPATIAL_PARTITION_CEILS];
+        newNode->surface = surface;
+        newNode->next = list->next;
+        list->next = newNode;
+    }
+    
+    if (normalY > 0.f) {
+        register struct SurfaceNode *newNode = alloc_surface_node();
+        register struct SurfaceNode *list = &cell[SPATIAL_PARTITION_FLOORS];
+        newNode->surface = surface;
+        newNode->next = list->next;
+        list->next = newNode;
+    }
+    
+    if (absf(normalY) <= 0.05f) {
+        register struct SurfaceNode *newNode = alloc_surface_node();
+        register struct SurfaceNode *list = &cell[SPATIAL_PARTITION_WALLS];
+        newNode->surface = surface;
+        newNode->next = list->next;
+        list->next = newNode;
+        
+        if (absf(surface->normal.x) > 0.707f)
+            surface->flags |= SURFACE_FLAG_X_PROJECTION;
+    }
 }
